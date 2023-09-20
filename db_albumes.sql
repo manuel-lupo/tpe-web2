a2b16d419dcabbd96e54f3121d50b8e5054c5795
Manuel Lupo <mlupo@alumnos.exa.unicen.edu.ar>
2023-09-20 18:53:22 -0300

    Commit inicial

---
+++ db_albumes.sql
@@ -0,0 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 20, 2023 at 06:46 PM
+-- Server version: 8.0.34-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2-1ubuntu2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_albumes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Albums`
+--
+
+CREATE TABLE `Albums` (
+  `id` int NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `rel_date` datetime DEFAULT NULL,
+  `review` text NOT NULL,
+  `artist_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Artists`
+--
+
+CREATE TABLE `Artists` (
+  `id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `nationality` varchar(45) DEFAULT NULL,
+  `birthday` datetime DEFAULT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Comments`
+--
+
+CREATE TABLE `Comments` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `comment` tinytext NOT NULL,
+  `rating` int NOT NULL,
+  `album_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Songs`
+--
+
+CREATE TABLE `Songs` (
+  `id` int NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `rel_date` datetime DEFAULT NULL,
+  `album_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Albums`
+--
+ALTER TABLE `Albums`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `artist_id` (`artist_id`);
+
+--
+-- Indexes for table `Artists`
+--
+ALTER TABLE `Artists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Comments`
+--
+ALTER TABLE `Comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `album_id` (`album_id`);
+
+--
+-- Indexes for table `Songs`
+--
+ALTER TABLE `Songs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `album_id` (`album_id`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Albums`
+--
+ALTER TABLE `Albums`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Artists`
+--
+ALTER TABLE `Artists`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Comments`
+--
+ALTER TABLE `Comments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Songs`
+--
+ALTER TABLE `Songs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Albums`
+--
+ALTER TABLE `Albums`
+  ADD CONSTRAINT `Albums_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `Artists` (`id`);
+
+--
+-- Constraints for table `Comments`
+--
+ALTER TABLE `Comments`
+  ADD CONSTRAINT `Comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`),
+  ADD CONSTRAINT `Comments_ibfk_2` FOREIGN KEY (`album_id`) REFERENCES `Albums` (`id`);
+
+--
+-- Constraints for table `Songs`
+--
+ALTER TABLE `Songs`
+  ADD CONSTRAINT `Songs_ibfk_1` FOREIGN KEY (`album_id`) REFERENCES `Albums` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
