8c01e0617117a7fbf7c7e0fe42d81dd061541fa5
Manuel Lupo <mlupo@alumnos.exa.unicen.edu.ar>
2023-10-06 19:06:01 -0300

    Renderizado de main

--- app/views/main.view.php
+++ app/views/main.view.php
@@ -1,4 +1,9 @@
 <?php
 class Main_view{
-    
+    public function renderAlbums($albums){
+        require_once './template/header.phtml';
+        require_once './template/main_page_cards.phtml';
+        require_once './template/main_page_searchbar.phtml';
+        require_once './template/footer.phtml';
+    }
 }
--- router.php
+++ router.php
@@ -1,17 +1,28 @@
 <?php
+try {
+    require_once './app/objects/Album.php';
+require_once './app/controllers/main.controller.php';
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']));
 
+$main_controller = new Main_controller();
 
 $action = 'home'; // accion por defecto
 if (!empty( $_GET['action'])) {
     $action = $_GET['action'];
 }
 
+// 'home' --> $main_controller->showHome();
+
 $params = explode('/', $action);
 
 switch ($params[0]) { // en la primer posicion tengo la accion real
     case 'home':
+        $main_controller->showHome();
         break;
     default: 
+        $main_controller->show404();
         break;
+}
+} catch (\Throwable $th) {
+    echo $th;
 }
