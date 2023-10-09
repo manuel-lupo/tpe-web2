27a28e84ddf9c2713ad8870f22c40e5be69838e7
Gianluca Merendino <gmerendino@alumnos.exa.unicen.edu.ar>
2023-10-09 00:37:35 -0300

    se avanzo con el armado de la pagina canciones

--- app/views/songs.view.php
+++ app/views/songs.view.php
@@ -2,13 +2,13 @@
 require_once './app/objects/Song.php';
 
 class Songs_view {
-    public function searchSongs(){
-        require_once './template/songs_searchbar.phtml';
-    }
     public function renderSongs($songs){
         require_once './template/header.phtml';
         require_once './template/songs_page.phtml';
         require_once './template/songs_searchbar.phtml';
         require_once './template/footer.phtml';
+    }
+    public function searchSongs(){
+        require_once './template/songs_searchbar.phtml';
     }
 }
--- router.php
+++ router.php
@@ -5,7 +5,7 @@ try {
     require_once './app/controllers/songs.controller.php';
     require_once './app/controllers/album.controller.php';
 
-    define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']));
+    define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
     $songs_controller = new songs_controller();
     $main_controller = new Main_controller();
     $album_controller = new Album_controller();
