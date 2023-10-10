bd6ad6797613b74701b9ab66c99cbed780e77832
Manuel Lupo <mlupo@alumnos.exa.unicen.edu.ar>
2023-10-10 14:56:07 -0300

    Añadido require de helper

--- app/controllers/auth.controller.php
+++ app/controllers/auth.controller.php
@@ -1,6 +1,7 @@
 <?php
 require_once './app/models/user.model.php';
 require_once './app/views/auth.view.php';
+require_once './app/helpers/auth.helper.php';
 
 class Auth_controller{
     private $model;
