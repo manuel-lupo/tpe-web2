f452349007361f102f8c99be1b819f38bf2bb9d4
Manuel Lupo <mlupo@alumnos.exa.unicen.edu.ar>
2023-10-10 12:09:03 -0300

    Corregido bug en el que el login elimina los eventos de las filas de la tabla

--- js/card_display.js
+++ js/card_display.js
@@ -1,28 +1,19 @@
-let CARD_DISPLAY = document.querySelector('.card_container');
-let ALBUM_ROWS = document.querySelectorAll('#album_row');
-ALBUM_ROWS.forEach(row => {
-    handleListeners(row);
-});
-
-function handleListeners(row){
-    row.addEventListener('click' , function(e){
-        let CARD_DISPLAY = document.querySelector('.card_container');
-        params = new URLSearchParams();
-        params.append('id' , this.dataset.id);
-        params.append('type', 'get_album_card');
-        fetch(`request` , {
-            method: 'POST',
-            body: params
+function handleListeners(){
+    let ALBUM_ROWS = document.querySelectorAll('#album_row');
+    ALBUM_ROWS.forEach(row => {
+        row.addEventListener('click' , function(e){
+            let CARD_DISPLAY = document.querySelector('.card_container');
+            params = new URLSearchParams();
+            params.append('id' , this.dataset.id);
+            params.append('type', 'get_album_card');
+            fetch(`request` , {
+                method: 'POST',
+                body: params
+            })
+            .then(response => response.text())
+            .then(html => CARD_DISPLAY.innerHTML = html);
         })
-        .then(response => response.text())
-        .then(html => CARD_DISPLAY.innerHTML = html);
-        document.addEventListener('login_closed', (e)=>{
-            let ALBUM_ROWS = document.querySelectorAll('#album_row');
-            ALBUM_ROWS.forEach(row => {
-                handleListeners(row);
-            });
-            e.stopImmediatePropagation();
-        })
-    })
+    });
+    document.addEventListener('login_closed', handleListeners);
 }
-
+handleListeners()
